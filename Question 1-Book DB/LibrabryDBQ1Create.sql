d8ad8084aab257a666e9d0523396eca6def09858
TheJiraiyaSama <rohitkane001@gmail.com>
2024-01-20 01:27:32 +0530

    "Completed"

---
+++ Question 1-Book DB/LibrabryDBQ1Create.sql
@@ -0,0 +1,45 @@
+CREATE TABLE publisher(
+    name VARCHAR(20) PRIMARY KEY,
+    address VARCHAR(20),
+    phone NUMBER(10)
+);
+
+CREATE TABLE book(
+    book_id INT PRIMARY KEY,
+    title VARCHAR(20),
+    publisher_name VARCHAR(20) REFERENCES publisher(name) ON DELETE SET NULL,
+    pub_year INT
+);
+
+CREATE TABLE book_author(
+    book_id INT REFERENCES book(book_id) ON DELETE CASCADE,
+    author_name VARCHAR(20)
+);
+
+CREATE TABLE library_programme(
+    programme_id INT PRIMARY KEY,
+    programme_name VARCHAR(20) NOT NULL,
+    address VARCHAR(20)
+);
+
+
+CREATE TABLE book_copies(
+    book_id INT REFERENCES book(book_id) ON DELETE CASCADE,
+    programme_id INT REFERENCES library_programme(programme_id) ON DELETE SET NULL,
+    no_of_copies INT
+);
+
+CREATE TABLE book_lending(  
+    book_id INT REFERENCES book(book_id) ON DELETE CASCADE,
+    programme_id INT REFERENCES library_programme(programme_id) ON DELETE SET NULL,
+    card_no INT,
+    date_out DATE,
+    due_date DATE
+);
+
+DELETE book_lending;
+DELETE book_copies;
+DELETE library_programme;
+DELETE book_author;
+DELETE book;
+DELETE publisher;
---
+++ Question 1-Book DB/LibraryDBQ1Queries.sql
@@ -0,0 +1,77 @@
+/*
+1. Retrieve details of all books in the library – id, title, name of publisher, authors, number of copies in each Programme, etc.
+*/
+
+SELECT B.book_id, B.title, B.publisher_name, BA.author_name, LP.programme_name, BC.no_of_copies
+    FROM book B, book_author BA, library_programme LP, book_copies BC
+    WHERE 
+        B.book_id = BA.book_id 
+            AND 
+        B.book_id = BC.book_id 
+            AND 
+        BC.programme_id = LP.programme_id;
+
+/*
+2. Get the particulars of borrowers who have borrowed more than 3 books, but from Jan 2017 to Jun 2017.
+*/
+
+SELECT card_no, COUNT(card_no)
+    FROM book_lending
+    WHERE date_out BETWEEN '01-Jan-2017' AND '01-Jun-2017'
+    HAVING COUNT(card_no) >= 3
+    GROUP BY card_no;
+    
+/*
+3. Delete a book in BOOK table. Update the contents of other tables to reflect this data manipulation operation
+*/
+DELETE 
+    FROM book
+    WHERE book_id = 1;
+
+/*
+4. Partition the BOOK table based on year of publication. Demonstrate its working with a simple query
+*/
+--observe value of year count in year 2016
+SELECT book_id, title, publisher_name, pub_year, COUNT(pub_year) OVER(PARTITION BY pub_year) YearCount
+    FROM book
+    ORDER BY pub_year;
+
+CREATE TABLE unpart_book AS Select * From book;
+SELECT * FROM unpart_book PARTITION (year_2015);
+
+--For Oracle 11C+ --
+ALTER TABLE unpart_book MODIFY PARTITION BY RANGE (pub_year) INTERVAL(1) (
+    PARTITION year_2015 VALUES LESS THAN (2016)
+);
+
+SELECT * FROM unpart_book PARTITION FOR (2016);
+
+--For Laboratory(Oracle 10g EE) --
+CREATE TABLE year_2016 AS 
+    SELECT * FROM book WHERE pub_year = 2016;
+SELECT * FROM year_2016;
+DROP TABLE year_2016;
+
+
+/*
+5. Create a view of all books and its number of copies that are currently available in the Library.
+*/
+--Partition by title to create a subview and also to use the aggregate SUM and also display the other fields, DISTINCT to not repeat.
+CREATE VIEW total_books AS
+    SELECT DISTINCT B.book_id, B.title, B.publisher_name, B.pub_year, SUM(BC.no_of_copies) OVER(PARTITION BY B.title) TotalBooks
+        FROM book B, book_copies BC
+        WHERE B.book_id = BC.book_id
+        ORDER BY B.book_id;
+DROP VIEW total_books;
+
+CREATE VIEW remaining_books AS
+    SELECT book_id, SUM(total_books) as total_copies, SUM(lend_books) as borrowed_books, (SUM(total_books)-SUM(lend_books)) as remaining
+        FROM (
+            SELECT book_id, no_of_copies as total_books, 0 as lend_books from book_copies 
+                UNION ALL
+            SELECT DISTINCT book_id, null as total_books, COUNT(card_no) as lend_books from book_lending group by(book_id)
+        )
+        GROUP BY book_id
+        ORDER BY book_id;
+SELECT * FROM remaining_books;
+DROP VIEW remaining_books;
